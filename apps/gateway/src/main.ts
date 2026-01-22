160e8a22ec845899efffee3cdfb7cfc087171860
silvio gori <silvioxd12@gmail.com>
2026-01-22 15:07:59 +0100

    feat: add swagger documentation

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Backend Challenge API')
+    .setDescription('The Gateway API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   console.log('Gateway is listening on HTTP port 3000');
+  console.log('Swagger is available at: http://localhost:3000/api');
 }
 bootstrap();
--- libs/common/src/dto/create-user.dto.ts
+++ libs/common/src/dto/create-user.dto.ts
@@ -1,13 +1,24 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'Dev', description: 'The name of the user' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 'test@example.com',
+    description: 'The email of the user',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'secret123',
+    description: 'he password of the user',
+    minLength: 6,
+  })
   @IsString()
   @MinLength(6)
   password: string;
