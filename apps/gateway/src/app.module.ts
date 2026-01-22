709c56ae30cf55f7711e318a4923beb3e21b1201
silvio gori <silvioxd12@gmail.com>
2026-01-22 19:20:29 +0100

    feat: add rate limiting to gateway

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { CommonConfigModule } from '@app/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     CommonConfigModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     CacheModule.register({
       isGlobal: true,
       ttl: 10000,
@@ -14,6 +22,11 @@ import { CommonConfigModule } from '@app/config';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
