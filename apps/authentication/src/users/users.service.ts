a99ded8ad8a710d4fa25e46ce59185961829685d
silvio gori <silvioxd12@gmail.com>
2026-01-22 14:56:15 +0100

    feat: implement password hashing for security

--- apps/authentication/src/users/users.service.ts
+++ apps/authentication/src/users/users.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from '@app/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async createUser(data: CreateUserDto) {
-    return this.usersRepository.create(data);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(data.password, salt);
+    const userToSave = {
+      ...data,
+      password: hashedPassword,
+    };
+
+    return this.usersRepository.create(userToSave);
   }
 
   async getUsers() {
